616f8cdde0968d3799665055a52fc0b0710c90d9
Lee Millward <lee.millward@gmail.com>
2021-01-23 21:17:25 +0000

    Slightly shrink size of logged in user icon on top nav

--- GolfDashboard.Web/src/components/logged-in-user.tsx
+++ GolfDashboard.Web/src/components/logged-in-user.tsx
@@ -6,7 +6,7 @@ export class LoggedInUser extends React.Component {
     render() {
         return (
             <div className="pr-3 ml-auto d-flex">
-                <img src={PersonLogo} alt="Person" />
+                <img src={PersonLogo} alt="Person" width="20" />
                 <p className="text-white-tinted pl-2 m-0 align-self-center">Lee</p>
             </div>
         );
