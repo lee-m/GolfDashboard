c57dad44ef7f63e73089e643b88bf1651227639b
Lee Millward <lee.millward@gmail.com>
2021-04-16 15:00:25 +0100

    Add support for applying custom CSS classes to a button component

--- GolfDashboard.Web/src/components/button.tsx
+++ GolfDashboard.Web/src/components/button.tsx
@@ -2,12 +2,13 @@ export interface ButtonProps {
     clickHandler: () => void,
     text?: string,
     disabled: boolean
-    outline?: boolean
+    outline?: boolean,
+    cssClasses?: string
 }
 
 export function Button(props: ButtonProps) {
 
-    let cssClasses = ["self-center", "px-4", "py-2", "text-sm", "font-semibold", "rounded", "disabled:cursor-not-allowed"];
+    let cssClasses = ["self-center", "px-4", "py-2", "text-xs", "font-semibold", "rounded", "disabled:cursor-not-allowed"];
     
     if(props.outline) {
         cssClasses.push("border", "border-2", "border-gray-400", "hover:bg-gray-200", "disabled:opacity-75");
@@ -15,7 +16,7 @@ export function Button(props: ButtonProps) {
         cssClasses.push("bg-secondary-500", "border-secondary-500", "text-white", "hover:bg-secondary-600", "disabled:opacity-50");
     }
     return (
-        <button className={cssClasses.join(" ")} onClick={props.clickHandler} disabled={props.disabled}>
+        <button className={props.cssClasses + " " + cssClasses.join(" ")} onClick={props.clickHandler} disabled={props.disabled}>
             {props.text}
         </button>
     );
