c82983a472ec497fa818f5b41dc43c7ed95915e1
Lee Millward <lee.millward@gmail.com>
2021-05-03 21:44:50 +0100

    Replace the tag box in the notes filter component with a list of tags
    First step to restoring the ability to delete tags

--- GolfDashboard.Web/src/pages/notes/notes-filter.tsx
+++ GolfDashboard.Web/src/pages/notes/notes-filter.tsx
@@ -1,6 +1,5 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Button from 'devextreme-react/button'
-import TagBox from 'devextreme-react/tag-box';
 
 import { Separator } from '../../components';
 import { NotesContext } from '../notes';
@@ -14,20 +13,36 @@ interface NotesFilterProps {
 export function NotesFilter(props: NotesFilterProps) {
 
     const notesContext = useContext(NotesContext);
-    const tagsDataSource = notesContext.tags.map(t => t.text);
+    const [selectedTags, setSelectedTags] = useState<Set<string>>(new Set<string>());
+
+    const onTagClick = (tag: string) => {
+
+        let newTags: Set<string>;
+
+        if (selectedTags.has(tag)) {
+            newTags = new Set([...selectedTags].filter(t => t != tag));
+        } else {
+            newTags = new Set([...selectedTags, tag]);
+        }
+
+        setSelectedTags(newTags);
+        props.updateFilter([...newTags]);
+    };
 
     return (
         <div className={"notes-filter" + (!props.visible ? "hidden" : "")}>
             <div className="flex pb-2">
                 <Button text="Add New Note" onClick={() => props.addNote()} disabled={false} stylingMode="contained" type="default" />
                 <span className="font-semibold self-center pl-2 pr-2 mb-0">Filter by Tag:</span>
-                <TagBox dataSource={tagsDataSource}
-                    showSelectionControls={true}
-                    showClearButton={true}
-                    applyValueMode="useButtons"
-                    stylingMode="underlined"
-                    elementAttr={{ class: "w-auto min-w-1/4" }}
-                    onValueChanged={(e) => props.updateFilter(e.value)} />
+                <div className="space-x-2">
+                    {notesContext.tags.map((t, i) => {
+                        return (
+                            <div className="dx-tag cursor-pointer shadow" key={i}>
+                                <span className={"dx-tag-content p-2 m-0 " + (selectedTags.has(t.text) ? "bg-secondary-300" : "")} onClick={() => onTagClick(t.text)}>{t.text}</span>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
             <Separator />
         </div>
