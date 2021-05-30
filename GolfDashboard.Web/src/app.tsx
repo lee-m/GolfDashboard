cafa45701248b58ef179b1386fc86164a397f98f
Lee Millward <lee.millward@gmail.com>
2021-05-30 11:01:27 +0100

    Convert app.tsx to a functional component

--- GolfDashboard.Web/src/app.tsx
+++ GolfDashboard.Web/src/app.tsx
@@ -3,29 +3,28 @@ import { Route, Switch } from 'react-router-dom';
 
 import { TopNavBar } from './navbar'
 import { Sidebar } from './sidebar';
-import { ClubsPage, DashboardPage, RoundsPage, NotesPage } from "./pages";
+import { ClubsPage, EditClubPage, DashboardPage, RoundsPage, NotesPage } from "./pages";
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './css/app.css';
 
-export default class App extends React.Component {
+export default function App(props: any) {
 
-  render() {
-    return (
-      <div className="root-content w-full overflow-hidden bg-primary-800">
-        <TopNavBar />
-        <Sidebar />
-        <div id="page-content" className="relative flex flex-col bg-gray-100 rounded-tl">
-          <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar={true} />
-          <Switch>
-            <Route exact path="/" component={DashboardPage} />
-            <Route path="/clubs" component={ClubsPage} />
-            <Route path="/rounds" component={RoundsPage} />
-            <Route path="/notes" component={NotesPage} />
-          </Switch>
-        </div>
+  return (
+    <div className="root-content w-full overflow-hidden bg-primary-800">
+      <TopNavBar />
+      <Sidebar />
+      <div id="page-content" className="relative flex flex-col bg-gray-100 rounded-tl">
+        <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar={true} />
+        <Switch>
+          <Route exact path="/" component={DashboardPage} />
+          <Route path="/clubs" component={ClubsPage} />
+          <Route path="/clubs/edit/:id" component={EditClubPage} />
+          <Route path="/rounds" component={RoundsPage} />
+          <Route path="/notes" component={NotesPage} />
+        </Switch>
       </div>
-    );
-  }
+    </div>
+  );
 }
